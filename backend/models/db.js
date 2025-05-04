5eb3b2fff0c262e2f2b0365bea97a76d38918cde
Uj <163011394+Uyrjn@users.noreply.github.com>
2025-05-04 15:59:16 +0700

    Delete backend directory

--- backend/models/db.js
+++
@@ -1,7 +0,0 @@
-// ==== /backend/models/db.js ====
-const mysql = require('mysql2');
-const config = require('../config');
-
-const pool = mysql.createPool(config.db);
-
-module.exports = pool.promise();
